c3a8d6034cc84dfc5dd88ce37d69d9a08c9be516
Noah Mattocks <noahmattocks@gmail.com>
2025-05-15 15:42:30 -0400

    Initial Commit

---
+++ WeatherAppDemo/WeatherAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppDemoApp.swift
+//  WeatherAppDemo
+//
+//  Created by Owner on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
